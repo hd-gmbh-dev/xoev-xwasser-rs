2add9ee6ec13e7d684f8cbcb456e68b8ad0fd36a
Jürgen Seitz <juergen.seitz@h-d-gmbh.de>
2024-08-02 16:43:57 +0200

    added xml validation in typescript tests via @raxb/validate-wasm

--- tests/quality_report_minimal.test.ts
+++ tests/quality_report_minimal.test.ts
@@ -9,14 +9,26 @@ import quality_report_minimal from './quality_report_minimal.json'
 import fs from 'fs'
 import path from 'path'
 const __dirname = import.meta.dirname;
+import xmlvalidate, { XmlValidatorError } from '@raxb/validate-wasm'
+const xsdBundle = fs.readFileSync(path.resolve(__dirname, '../target/schemas/out/980faa7cf81d51eab3f26de770ae345d.xsdb')).buffer;
 
 describe("simple xml generation via wasm", async () => {
+  const { XmlValidator } = await xmlvalidate();
+  const validator = new XmlValidator(new Uint8Array(xsdBundle));
+  validator.init((err: string) => {
+    console.error(err);
+  });
+
   it("should be able to create quality report xml", async () => {
     const xml = create_vorgang_transportieren_2010(quality_report_minimal as any as VorgangTransportieren2010)
       .replace("https://gitlab.opencode.de/akdb/xoev/xwasser/-/raw/main/V0_5_0 xwasser.xsd", "https://gitlab.opencode.de/akdb/xoev/xwasser/-/raw/main/V0_5_0 ../schemas/V0_5_0/xwasser.xsd");
     const expected_xml = fs.readFileSync(path.resolve(__dirname, './quality_report_minimal_test_result.xml'), 'utf-8');
     expect(xml).to.equal(expected_xml);
+    validator.validateXml(xml, (err: XmlValidatorError) => {
+      console.log({ level: err.level, line: err.line, message: err.message });      
+    })    
   });
+
   it("should be able to parse quality report xml", async () => {
     const source = fs.readFileSync(path.resolve(__dirname, './quality_report_minimal.xml'), 'utf-8');
     const obj = parse_vorgang_transportieren_2010(source)
