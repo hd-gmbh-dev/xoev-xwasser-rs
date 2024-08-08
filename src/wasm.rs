4c1c9c5ea76d7e6fe23a6cd63669e52eadce4563
Alexander Korolev <alexander.korolev.germany@gmail.com>
2024-08-09 00:21:04 +0200

    added administration receipt (administration.quittung.0020)

--- src/wasm.rs
+++ src/wasm.rs
@@ -1,5 +1,8 @@
-use crate::model::transport::VorgangTransportieren2010;
 use wasm_bindgen::prelude::*;
+
+use crate::model::{
+    administration::AdministrationQuittung0020, transport::VorgangTransportieren2010,
+};
 
 #[wasm_bindgen]
 pub fn create_vorgang_transportieren_2010(
@@ -12,6 +15,24 @@ pub fn create_vorgang_transportieren_2010(
 pub fn parse_vorgang_transportieren_2010(
     xml: String,
 ) -> Result<VorgangTransportieren2010, JsValue> {
+    use raxb::quick_xml::NsReader;
+
+    let mut rdr = NsReader::from_str(&xml);
+    rdr.config_mut().trim_text(true);
+    raxb::de::deserialize_with_reader(rdr).map_err(|err| JsValue::from_str(&err.to_string()))
+}
+
+#[wasm_bindgen]
+pub fn create_administration_quittung_0020(
+    data: AdministrationQuittung0020,
+) -> Result<String, JsValue> {
+    raxb::ser::to_string_pretty_with_decl(&data).map_err(|err| JsValue::from_str(&err.to_string()))
+}
+
+#[wasm_bindgen]
+pub fn parse_administration_quittung_0020(
+    xml: String,
+) -> Result<AdministrationQuittung0020, JsValue> {
     use raxb::quick_xml::NsReader;
 
     let mut rdr = NsReader::from_str(&xml);
